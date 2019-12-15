46399726fd15287b4d7a63bb73a3b38e0674f304
kelly r maass <kmaass77627@gmail.com>
2019-12-15 19:50:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,8 +8,8 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergea
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female', 'black', 'calm', 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'male', 'brown', 'calm', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'male', 'brown', 'calm', 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'female', 'brown', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'female', 'brown', 'calm', 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9, 'male', 'brown', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9, 'male', 'brown', 'calm', 0);
