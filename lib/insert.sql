b0185c10ec6321a423412a409745b4c9a79a99ab
kelly r maass <kmaass77627@gmail.com>
2019-12-15 19:46:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
 INSERT INTO bears(Mr.Chocolate, 5, 'male', 'brown', 'calm', true);
 
-INSERT INTO bears ()('Rowdy', 3, 'male', 'black', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'black', 'calm', true);
 
-INSERT INTO bears('Tabitha', 7, 'female', 'white', 'calm', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'female', 'white', 'calm', false);
 
-INSERT INTO bears('Sergeant Brown', 2, 'male', 'brown', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'male', 'brown', 'calm', true);
 
-INSERT INTO bears('Melissa', 8, 'female', 'black', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female', 'black', 'calm', true);
 
-INSERT INTO bears('Grinch', 1, 'male', 'brown', 'calm', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'male', 'brown', 'calm', false);
 
-INSERT INTO bears('Wendy', 9, 'female', 'brown', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'female', 'brown', 'calm', true);
 
-INSERT INTO bears(NULL, 9, 'male', 'brown', 'calm', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9, 'male', 'brown', 'calm', true);
