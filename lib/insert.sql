d08cde81cb237e14e9c60c984d446a8cc5e81900
kelly r maass <kmaass77627@gmail.com>
2019-12-15 19:32:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,15 @@
-INSERT INTO bears(MR.Chocolate, 5, male, brown, calm, true);
+INSERT INTO bears(Mr.Chocolate, 5, male, brown, calm, true);
+
+INSERT INTO bears(Rowdy, 3, male, brown, calm, true);
+
+INSERT INTO bears(Tabitha, 7, male, brown, calm, true);
+
+INSERT INTO bears(Sergeant Brown, 2, male, brown, calm, true);
+
+INSERT INTO bears(Melissa, 8, male, brown, calm, true);
+
+INSERT INTO bears(Grinch, 1, male, brown, calm, false);
+
+INSERT INTO bears(Wendy, 9, male, brown, calm, true);
+
+INSERT INTO bears(NULL, 9, male, brown, calm, true);
